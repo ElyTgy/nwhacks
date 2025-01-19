760630721ed8d5c287996a3c8b232214066233b6
nickkkk1234 <nicolasdaguila40@gmail.com>
2025-01-18 17:18:44 -0800

    fonts + ready for dev

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
+      <h3>sigma</h3>
+      <main className="flex flex-col gap-8 row-start-2 items-center">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,11 +13,11 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-center">
           <li className="mb-2">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5">
+              app/page.tsx
             </code>
             .
           </li>
