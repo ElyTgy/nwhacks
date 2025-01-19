811090e48dd55942ee09deb83c9f2b95e321eed9
Anush Mutyala <mutyalaanush@gmail.com>
2025-01-18 22:23:48 -0800

    test spotify

--- spotify/spotify_test.py
+++ spotify/spotify_test.py
@@ -1,23 +1,28 @@
 from dotenv import load_dotenv
-from flask import Flask, redirect
+from flask import Flask, redirect, request, jsonify, session
+from datetime import datetime, timedelta
 import urllib.parse
 import requests
 import os
 
 load_dotenv()
+
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 secret_key = os.getenv("SECRET_KEY")
  
-REDIRECT_URL = "http://127.0.0.1:9090/callback"
+REDIRECT_URL = "http://localhost:8000/callback"
 AUTH_URL = "https://accounts.spotify.com/authorize"
 TOKEN_URL = "https://accounts.spotify.com/api/token" #for refreshing token
 API_BASE_URL = "https://api.spotify.com/v1/"
 
 
+app = Flask(__name__)
+app.secret_key = secret_key
+
 @app.route('/')
 def index():
-    return "Welcome to the spotify"
+    return "Welcome to the spotify <a href='/login'>Login with spotify</a>'"
 
 
 @app.route('/login')
@@ -41,9 +46,9 @@ def login():
 def callback():
     if 'error' in request.args:
         return jsonify({"error": request.args['error']})
-    if 'code' in requests.args:
+    if 'code' in request.args:
         req_body = {
-            'code': request.args['c'],
+            'code': request.args['code'],
             'grant_type': 'authorization_code',
             'redirect_uri': REDIRECT_URL,
             'client_id': client_id,
@@ -54,7 +59,7 @@ def callback():
 
         session['access_token'] = token_info['access_token']
         session['refresh_token'] = token_info['refresh_token']
-        session['expires_at'] = datetime.now().timestamp() + token_info['expires_at']
+        session['expires_at'] = datetime.now().timestamp() + token_info['expires_in']
 
         return redirect('/dashboard')
 
@@ -67,7 +72,7 @@ def dashboard():
         return redirect('/refresh_token')
     
     headers = {
-        'Authorization': f"Bearer ${session["access_token"]}",
+        'Authorization': f"Bearer {session['access_token']}",
     }
 
     response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
@@ -76,7 +81,7 @@ def dashboard():
     return jsonify(playlists)
 
 
-@app.route('refresh-token')
+@app.route('/refresh-token')
 def refresh_token():
     if 'refresh_token' not in session:
         return redirect('/login')
@@ -92,7 +97,11 @@ def refresh_token():
         new_token_info = response.json
         
         session['access_token'] = new_token_info['access_token']
-        session['expires_at'] = datetime.now().timestamp() + token_info['expires_at']
+        session['expires_at'] = datetime.now().timestamp() + new_token_info['expires_in']
 
-app = Flask(__name__)
-app.secret_key = secret_key
+        return redirect('/dashboard')
+    
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000, debug=True)
+
